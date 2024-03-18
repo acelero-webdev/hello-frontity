e999a198de9fbc660db1bdbbe62666c5a5ce98f0
acelero-webdev <webdev@acelero.net>
2024-03-17 23:15:28 -0500

    dynamically retrieve page data and use it in our root element

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -3,21 +3,32 @@
 import React from "react";
 import { connect } from "frontity";
 import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
 
 const Root = ({ state }) => {
-  return (
-    <>
-      <h1>Hello Frontity</h1>
-      <p>Current URL: { state.router.link }</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <br />
-        <Link link="/page/2">More posts</Link>
-        <br />
-        <Link link="/about-us">About Us</Link>
-      </nav>
-    </>
-  )
+    const data = state.source.get(state.router.link);
+    console.log(data);
+
+    return (
+        <>
+        <h1>Hello Frontity</h1>
+        <p>Current URL: { state.router.link }</p>
+        <nav>
+            <Link link="/">Home</Link>
+            <br />
+            <Link link="/page/2">More posts</Link>
+            <br />
+            <Link link="/about-us">About Us</Link>
+        </nav>
+        <main>
+            <Switch>
+                <div when={data.isArchive}>This is a list.</div>
+                <div when={data.isPost}>This is a post.</div>
+                <div when={data.isPage}>This is a page.</div>
+            </Switch>
+        </main>
+        </>
+    )
 }
 
 export default connect(Root);
